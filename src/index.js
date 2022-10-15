59e21f90995357dcaa4ec264f5344f7e84f81f45
Irina Khorunz <irahorunz87@gmail.com>
2022-10-15 22:59:31 +0300

    add markup card

--- src/index.js
+++ src/index.js
@@ -13,12 +13,31 @@ inputEl.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 function handleInput() {
   const countryName = inputEl.value.trim();
 
-  fetchCountries(countryName)
-    .then(data => {
-      const markup = createMarkupList(data);
-      listEl.innerHTML = markup;
-    })
-    .catch(err => {});
+  if (countryName === '') {
+    listEl.innerHTML = '';
+  } else {
+    fetchCountries(countryName)
+      .then(data => {
+        console.log(data);
+
+        if (data.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          listEl.innerHTML = '';
+          return;
+        } else {
+          const markup =
+            data.length === 1 ? createMarkupCard(data) : createMarkupList(data);
+
+          listEl.innerHTML = markup;
+        }
+      })
+      .catch(err => {
+        Notify.failure('Oops, there is no country with that name');
+        listEl.innerHTML = '';
+      });
+  }
 }
 
 function createMarkupList(arr) {
@@ -28,6 +47,20 @@ function createMarkupList(arr) {
   <img src="${country.flags.svg}" alt="flag ${country.name.official}" width = "30" />
   <h2 class="country-title">${country.name.official}</h2>
   </li>`
+    )
+    .join('');
+}
+
+function createMarkupCard(arr) {
+  return arr
+    .map(
+      country => `<img src="${country.flags.svg}" alt="flag ${
+        country.name.official
+      }" width = "30" />
+  <h2>${country.name.official}</h2>
+  <p>Capital: ${country.capital}</p>
+  <p>Population: ${country.population}</p>
+  <p>Languages: ${(Object.values(country.languages)).join(', ')}</p>`
     )
     .join('');
 }
